37589d6cc3eb1880512482b11aa913a0ad8a505a
simon <923098122@qq.com>
2021-12-12 17:39:15 +0800

    add sharding-sphere

---
+++ demo-sharding-sphere/src/main/java/com/example/demoshardingsphere/DemoShardingSphereApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoshardingsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShardingSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoShardingSphereApplication.class, args);
+    }
+
+}
